7faf2421c2f1db30f072aeff8ef3add2efc6683f
Al Va <102867612+alva922@users.noreply.github.com>
2024-03-24 22:52:01 +0100

    Create pycodes100_04_Descriptive_Statistics.py
    Descriptive Statistics of NBA

---
+++ pycodes100_04_Descriptive_Statistics.py
@@ -0,0 +1,5 @@
+#Showing Descriptive Statistics
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#showing-descriptive-statistics
+#To get an overview of the values each column contains, you can use .describe():
+nba.describe().T
+#See descriptive_statistics.PNG
