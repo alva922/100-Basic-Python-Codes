0e03289ff919e3e82cdc675f37bf7253ac352349
Al Va <102867612+alva922@users.noreply.github.com>
2024-03-24 14:30:25 +0100

    Create pycodes100_0.py
    Setting Up Your Environment

---
+++ pycodes100_0.py
@@ -0,0 +1,11 @@
+#Setting Up Your Environment
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#setting-up-your-environment
+import os
+os.chdir('YOURPATH')    # Set working directory
+os. getcwd()
+#You’ll need pandas and other Python libraries, which you can install with pip from cmd prompt
+python3 -m pip install requests pandas matplotlib
+#Within the Jupyter notebook:
+!pip install requests pandas matplotlib
+#You can also use the Conda package manager
+#conda install requests pandas matplotlib
