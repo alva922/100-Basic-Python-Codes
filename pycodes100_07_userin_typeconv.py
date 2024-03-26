7191930a99efcf97cbfc6c6130c6bae02c79d675
Al Va <102867612+alva922@users.noreply.github.com>
2024-03-26 16:32:35 +0100

    Create pycodes100_07_userin_typeconv.py
    User Input & Type Conversion

---
+++ pycodes100_07_userin_typeconv.py
@@ -0,0 +1,17 @@
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#user-input-type-conversion
+#Printing the read-in data from the user while performing type conversion
+name=input("what is your name? ")
+print("Hello {} !".format(name))
+print(type(name))
+num1=input("write a number")
+num1=int(num1)
+num=5
+type(num)
+result=num+num1
+print("{}+{}={}".format(num,num1,result))
+#Example I/O
+what is your name? Al
+Hello Al !
+<class 'str'>
+write a number15
+5+15=20
