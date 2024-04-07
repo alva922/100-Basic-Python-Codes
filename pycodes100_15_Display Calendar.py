4eeabaf48e49ed9e54da60bcc8c2284108dbe011
Al Va <alexzap922@gmail.com>
2024-04-07 19:25:04 +0200

    Create pycodes100_15_Display Calendar.py
    Display the calendar by importing the calendar module to our program

---
+++ pycodes100_15_Display Calendar.py
@@ -0,0 +1,22 @@
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#display-calendar
+#Display the calendar by importing the calendar module to our program
+import calendar
+ 
+#initialize the year and the month variables
+ 
+yr = 2024
+ 
+mnth = 2
+ 
+# printing the calendar
+ 
+print(calendar.month(yr, mnth))
+ 
+Output:
+   February 2024
+Mo Tu We Th Fr Sa Su
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29
