955b86716769232c545fe2ac458c594981dfc21d
Al Va <alexzap922@gmail.com>
2024-04-07 19:22:39 +0200

    Create pycodes14_Story Generator.py
    Story Generator

---
+++ pycodes14_Story Generator.py
@@ -0,0 +1,13 @@
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#story-generator
+#Generate random stories using the random module
+import random
+when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago','On 20th Jan']
+who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle','a cat']
+name = ['Ali', 'Miriam','daniel', 'Hoouk', 'Starwalker']
+residence = ['Barcelona','India', 'Germany', 'Venice', 'England']
+went = ['cinema', 'university','seminar', 'school', 'laundry']
+happened = ['made a lot of friends','Eats a burger', 'found a secret key', 'solved a mystery', 'wrote a book']
+print(random.choice(when) + ', ' + random.choice(who) + ' that lived in ' + random.choice(residence) + ', went to the ' + random.choice(went) + ' and ' + random.choice(happened))
+ 
+Output:
+On 20th Jan, a turtle that lived in Venice, went to the university and solved a mystery
