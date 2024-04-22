25c8b2410910e5fe26a9d02707e6731fa942f71e
Al Va <alexzap922@gmail.com>
2024-04-22 13:55:04 +0200

    Create pycodes100_40_slice_data.py
    Slice a string using 1, 2, and 3 parameters

---
+++ pycodes100_40_slice_data.py
@@ -0,0 +1,23 @@
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#slice-data
+#Slice Data
+# Slice a string using 1, 2, and 3 parameters
+# Assign string value
+text = "Python Programming Language"
+ 
+# Slice using one parameter
+sliceObj = slice(5)
+print(text[sliceObj])  
+ 
+# Slice using two parameter
+sliceObj = slice(6,12)
+print(text[sliceObj])  
+ 
+# Slice using three parameter
+sliceObj = slice(6,25,5)
+print(text[sliceObj])
+ 
+Output:
+ 
+Pytho
+ Progr
+ rnn
