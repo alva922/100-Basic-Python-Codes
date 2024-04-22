8d7c121ab83392c7748217f3459ee20c72889805
Al Va <alexzap922@gmail.com>
2024-04-22 13:05:54 +0200

    Create pycodes100_35_while_loop.py
    While Loop

---
+++ pycodes100_35_while_loop.py
@@ -0,0 +1,21 @@
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#while-loop
+#While Loop
+# Initialize counter
+counter = 1
+# Iterate the loop 9 times
+while counter < 10:
+    # Print the counter value
+    print ("%d" % counter)
+    # Increment the counter
+    counter = counter + 1
+ 
+Output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
