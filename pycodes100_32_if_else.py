30ccdfdbbce058e8e8846d2cac54ae5c27719060
Al Va <alexzap922@gmail.com>
2024-04-22 13:01:05 +0200

    Create pycodes100_32_if_else.py
    Example of the If Else Statement

---
+++ pycodes100_32_if_else.py
@@ -0,0 +1,12 @@
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#if-else-statement
+#Example of the If Else Statement
+# Assign a numeric value
+x = 35
+ 
+# Check the is more than 35 or not
+if (x >= 35):
+    print("You have passed")
+else:
+    print("You have not passed")
+Output:
+You have passed
