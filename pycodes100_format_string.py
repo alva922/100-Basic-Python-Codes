f130ccf622a43af5001300f2ae08268c7c4b2cfb
Al Va <alexzap922@gmail.com>
2024-04-22 12:55:40 +0200

    Create pycodes100_format_string.py
    Format floating point in the string using String Formatting

---
+++ pycodes100_format_string.py
@@ -0,0 +1,12 @@
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#format-floating-point-in-the-string
+#Format floating point in the string using String Formatting
+# Use of String Formatting
+x = 462.75897
+print("{:5.2f}".format(x))
+ 
+# Use of String Interpolation
+y = 462.75897
+print("%5.2f" % y)
+Output:
+462.76
+462.76
