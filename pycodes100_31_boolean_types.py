40e6c484a7bcd971b1f31c0026823d4e3046db97
Al Va <alexzap922@gmail.com>
2024-04-22 12:59:48 +0200

    Create pycodes100_31_boolean_types.py
    Working with Boolean Types

---
+++ pycodes100_31_boolean_types.py
@@ -0,0 +1,27 @@
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#working-with-boolean-types
+#Working with Boolean Types
+# Boolean value
+x = True
+print(x)
+ 
+# Number to Boolean
+x = 10
+print(bool(x))
+ 
+x = -5
+print(bool(x))
+ 
+x = 0
+print(bool(x))
+ 
+# Boolean from comparison operator
+x = 6
+y = 3
+print(x < y)
+ 
+Output:
+True
+True
+True
+False
+False
