1e80c3593f967a26087521f3aea46e8f874a8f45
Al Va <alexzap922@gmail.com>
2024-04-29 09:55:59 +0200

    Create pycodes100_42_count_items.py
    Count Items in the List

---
+++ pycodes100_42_count_items.py
@@ -0,0 +1,13 @@
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#count-items-in-the-list
+#Count items in the list
+# Define the string
+string = 'Python Go JavaScript HTML CSS MYSQL Python'
+# Define the search string
+search = 'Python'
+# Store the count value
+count = string.count(search)
+# Print the formatted output
+print("%s appears %d times" % (search, count))
+ 
+Output:
+Python appears 2 times
