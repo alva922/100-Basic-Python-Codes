2814189fdf0f2edfc0737d090b3f25a03a3db0ac
Al Va <alexzap922@gmail.com>
2024-04-29 10:07:11 +0200

    Create pycodes100_48_range.py
    Use of range Function

---
+++ pycodes100_48_range.py
@@ -0,0 +1,16 @@
+#https://newdigitals.org/2024/02/24/100-basic-python-codes/#use-of-range-function
+#Use of range Function
+# range() with one parameter
+for val in range(7):
+    print(val, end='  ')
+print('\n')
+ 
+# range() with two parameters
+for val in range(7,16):
+    print(val, end='  ')
+print('\n')
+ 
+Output:
+0  1  2  3  4  5  6 
+ 
+7  8  9  10  11  12  13  14  15
